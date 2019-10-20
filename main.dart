ebbda2d38f66f508dbf22380f7e6db18dc5a4ffa
Fisayo Obilaja <34406562+fiizzy@users.noreply.github.com>
2019-10-20 09:40:02 +0100

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'ui/weight_on_planet.dart';
+
+void main(){
+    runApp(
+    new MaterialApp(
+    title: "Weight on palnet X")
+    home: new WeightOnPlanet);
+}
